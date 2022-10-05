227d8f7e9aefef3ccb98a2cc1993757f7b8236f9
sukkie <sukkie512@yahoo.co.jp>
2022-10-05 13:50:33 +0900

    m

--- src/main/java/com/cos/reactivetest/MyPub.java
+++ src/main/java/com/cos/reactivetest/MyPub.java
@@ -13,6 +13,7 @@ public class MyPub implements Publisher<Integer> {
     public void subscribe(Subscriber<? super Integer> s) {
         System.out.println("구독자 : 신문사야 너희 신문 볼게");
         System.out.println("신문사 : 구독 정보 만들어서 돌려줄테니 기다려.");
+        // 1. Publisher에 본인이 소유할 Subscription을 구현하고 publishing할 data를 만듭니다.
         MySubscription mySubscription = new MySubscription(s, its);
         System.out.println("신문사 : 구독정보 생성 완료");
         s.onSubscribe(mySubscription);
--- src/main/java/com/cos/reactivetest/MySub.java
+++ src/main/java/com/cos/reactivetest/MySub.java
@@ -3,16 +3,23 @@ package com.cos.reactivetest;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
+// 5. Subscription의 request()에는 조건에 따라 Subscriber의 onNext(), onComplete() 또는 onError()를 호출합니다.
+// 그러면 Subscriber의 해당 메서드의 로직에 따라 request() 또는 cancel()로 제어하게 됩니다.
 public class MySub implements Subscriber<Integer> {
 
     private Subscription s;
     private int bufferSize = 2;
 
+    // 3. Subscriber는 onSubscribe() 메서드를 통해 Subscription을 등록하고 Publisher를 구독하기 시작합니다.
+    // 이는 Publisher에 구현된 Subscription을 통해 이루어집니다.
+    // 이렇게 하면 Publisher와 Subscriber는 Subscription을 통해 연결된 상태가 됩니다.
+    // onSubscribe() 내부에 Subscription의 request()를 요청하면 그때부터 data 구독이 시작됩니다.
     @Override
     public void onSubscribe(Subscription s) {
         this.s = s;
         System.out.println("구독자 : 구독정보 잘 받았어.");
         System.out.println("구독자 : 나 이제 신문 하나씩 줘");
+        // 4. Suscriber는 Subscription 메서드의 request() 또는 cancel()을 호출을 통해 data의 흐름을 제어할 수 있습니다.
         s.request(bufferSize);  // 신문 한개씩 매일매일 줘, 소비자가 한번에 처리할 수 있는 데이터 요청
 
     }
